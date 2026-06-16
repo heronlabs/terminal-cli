0995ac4f3fdaa0fc392ca0d98367fa1d4da9083d
Lucas Lacerda <lucaslacerdacl@gmail.com>
2026-06-16 08:33:43 -0300

    fix: mariadb-dump no-tablespaces flag

--- src/core/services/mysql/mysql-backup.sh
+++ src/core/services/mysql/mysql-backup.sh
@@ -1,2 +1,2 @@
 set -o pipefail
-mariadb-dump -u "$DB_USER" -h "$DB_HOST" -P "$DB_PORT" "$DB_NAME" | gzip > "$BACKUP_FILE"
+mariadb-dump --no-tablespaces -u "$DB_USER" -h "$DB_HOST" -P "$DB_PORT" "$DB_NAME" | gzip > "$BACKUP_FILE"
